dad9c0a30b48621a6dd752e7e22da2cba3c3dc65
vercetti1337 <dinar1498@icloud.com>
2018-10-08 11:09:37 +0300

    Data Analysis

--- WebApplication1/WebApplication1/Models/FileModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class FileModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
-    }
-}
